78b8687a2d23f657346891726de240db3537c528
shashank <sh.swami235@gmail.com>
2019-04-10 20:12:55 -0400

    Updating funky A* answer code.

--- graphs/day03/src/Board.java
+++ graphs/day03/src/Board.java
@@ -1,5 +1,8 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Board definition for the 8 Puzzle challenge
@@ -7,7 +10,9 @@ import java.util.List;
 public class Board {
 
     private int n;
+    public int[] zeropos = new int[]{-1, -1};
     public int[][] tiles;
+    private HashMap<Integer, Integer[]> sol = new HashMap<>();
 
     //TODO
     // Create a 2D array representing the solved board state
@@ -18,6 +23,24 @@ public class Board {
      */
     public Board(int[][] b) {
         // TODO: Your code here
+        // Define what n is
+        n = (b.length == b[0].length) ? b.length : 0;
+        tiles = b;
+        for (int p = 0; p < n; p++) {
+            for (int m = 0; m < n; m++) {
+                if (tiles[p][m] == 0) {
+                    zeropos = new int[]{p, m};
+                }
+            }
+        }
+        // System.out.println("This is the zeroth position: " + zeropos[0] + ", " + zeropos[1]);
+        int count = 1;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sol.put(count, new Integer[] {i, j});
+                count++;
+            }
+        }
     }
 
     /*
@@ -25,24 +48,32 @@ public class Board {
      (equal to 3 for 8 puzzle, 4 for 15 puzzle, 5 for 24 puzzle, etc)
      */
     private int size() {
-        // TODO: Your code here
-        return 0;
+        return n;
     }
 
     /*
      * Sum of the manhattan distances between the tiles and the goal
      */
     public int manhattan() {
-        // TODO: Your code here
-        return 0;
+        int dist = 0;
+        Integer[] solVal = new Integer[] {0,0};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                solVal = (tiles[i][j] != 0) ? sol.get(tiles[i][j]) : new Integer[]{i, j};
+                dist += Math.abs(solVal[0] - i) + Math.abs(solVal[1] - j);
+            }
+        }
+        return dist;
     }
 
     /*
      * Compare the current state to the goal state
      */
     public boolean isGoal() {
-        // TODO: Your code here
-        return false;
+        if (manhattan() > 0) {
+            return false;
+        }
+        return true;
     }
 
     /*
@@ -50,16 +81,155 @@ public class Board {
      * Research how to check this without exploring all states
      */
     public boolean solvable() {
-        // TODO: Your code here
+        /* int count = 0;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (tiles[i][j] > 0 && tiles[j][i] > 0 && tiles[j][i] > tiles[i][j])
+                    count++;
+            }
+        }
+        if (count%2 == 0)
+            return true;
+        return false; */
+        int count = 0;
+        int temp = 0;
+        for (int i = 0; i < 8; i++) {
+            temp = tiles[(int) i/3][i%3];
+            if (temp != 0) {
+                for (int j = i+1; j < 9; j++) {
+                    if (tiles[(int) j/3][j%3] != 0 && tiles[(int) j/3][j%3] < temp) {
+                        count++;
+                    }
+                }
+            }
+        }
+        if (count % 2 == 0) {
+            return true;
+        }
         return false;
+    }
+
+    private static int[][] copyOf(int[][] A) {
+        int[][] B = new int[A.length][A[0].length];
+        for (int i = 0; i < A.length; i++)
+            System.arraycopy(A[i], 0, B[i], 0, A[0].length);
+        return B;
     }
 
     /*
      * Return all neighboring boards in the state tree
      */
-    public Iterable<Board> neighbors() {
+    public Iterable<Board> neighbors(boolean fastQ) {
         // TODO: Your code here
-        return null;
+        ArrayList<Board> boo = new ArrayList<>();
+        if (fastQ) {
+            fastFindNeighbors(zeropos, 0, copyOf(tiles), boo);
+        }
+        else {
+            slowFindNeighbors(zeropos, 0, copyOf(tiles), boo);
+        }
+        Iterable<Board> foo = boo;
+        return foo;
+    }
+
+    private void slowFindNeighbors(int[] pos, int dir, int[][] bt, ArrayList<Board> board) {
+        int temp = 0;
+        switch(dir) {
+            case 0: // go up
+                if (pos[0] != 0) {
+                    temp = bt[pos[0]-1][pos[1]];
+                    bt[pos[0]-1][pos[1]] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                }
+                break;
+            case 1: // go down
+                if (pos[0] != n-1) {
+                    temp = bt[pos[0]+1][pos[1]];
+                    bt[pos[0]+1][pos[1]] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                }
+                break;
+            case 2: // go right
+                if (pos[1] != n-1) {
+                    temp = bt[pos[0]][pos[1]+1];
+                    bt[pos[0]][pos[1]+1] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                }
+                break;
+            case 3: // go left
+                if (pos[1] != 0) {
+                    temp = bt[pos[0]][pos[1]-1];
+                    bt[pos[0]][pos[1]-1] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                }
+                break;
+            default:
+                break;
+        }
+        switch(dir) {
+            case 3:
+                break;
+            default:
+                slowFindNeighbors(zeropos, dir + 1, copyOf(tiles), board);
+                break;
+        }
+    }
+
+    private void fastFindNeighbors(int[] pos, int dir, int[][] bt, ArrayList<Board> board) {
+        int temp = 0;
+        switch(dir) {
+            case 0: // go up
+                if (pos[0] != 0) {
+                    temp = bt[pos[0]-1][pos[1]];
+                    bt[pos[0]-1][pos[1]] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                    fastFindNeighbors(new int[]{pos[0]-1,pos[1]}, dir, bt, board);
+                }
+                break;
+            case 1: // go down
+                if (pos[0] != n-1) {
+                    temp = bt[pos[0]+1][pos[1]];
+                    bt[pos[0]+1][pos[1]] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                    fastFindNeighbors(new int[]{pos[0]+1,pos[1]}, dir, bt, board);
+                }
+                break;
+            case 2: // go right
+                if (pos[1] != n-1) {
+                    temp = bt[pos[0]][pos[1]+1];
+                    bt[pos[0]][pos[1]+1] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                    fastFindNeighbors(new int[]{pos[0],pos[1]+1}, dir, bt, board);
+                }
+                break;
+            case 3: // go left
+                if (pos[1] != 0) {
+                    temp = bt[pos[0]][pos[1]-1];
+                    bt[pos[0]][pos[1]-1] = bt[pos[0]][pos[1]];
+                    bt[pos[0]][pos[1]] = temp;
+                    board.add(new Board(copyOf(bt)));
+                    fastFindNeighbors(new int[]{pos[0],pos[1]-1}, dir, bt, board);
+                }
+                break;
+            default:
+                break;
+        }
+        if (pos[0] == zeropos[0] && pos[1] == zeropos[1]) {
+            switch(dir) {
+                case 3:
+                    break;
+                default:
+                    fastFindNeighbors(zeropos, dir + 1, copyOf(tiles), board);
+                    break;
+            }
+        }
     }
 
     /*
@@ -89,14 +259,21 @@ public class Board {
 
     public static void main(String[] args) {
         // DEBUG - Your solution can include whatever output you find useful
-        int[][] initState = {{1, 2, 3}, {4, 0, 6}, {7, 8, 5}};
+        int[][] initState = {{1, 2, 5}, {4, 3, 8}, {7, 6, 0}};
+
         Board board = new Board(initState);
 
         System.out.println("Size: " + board.size());
         System.out.println("Solvable: " + board.solvable());
         System.out.println("Manhattan: " + board.manhattan());
         System.out.println("Is goal: " + board.isGoal());
-        System.out.println("Neighbors:");
-        Iterable<Board> it = board.neighbors();
+        System.out.println("-----\nNeighbors:");
+        Iterable<Board> it = board.neighbors(true);
+        int c = 1;
+        for (Board i : it) {
+            System.out.println("Board " + c);
+            System.out.println(Arrays.deepToString(i.tiles));
+            c++;
+        }
     }
 }
--- graphs/day03/src/Solver.java
+++ graphs/day03/src/Solver.java
@@ -5,16 +5,25 @@
 
 import java.util.*;
 
+import javax.swing.plaf.nimbus.State;
+
 public class Solver {
 
     public int minMoves = -1;
     private State solutionState;
+    private State fin;
     private boolean solved = false;
 
     /**
      * State class to make the cost calculations simple
      * This class holds a board state and all of its attributes
      */
+    private class StateComparer implements Comparator<State> {
+        public int compare(State s1, State s2) {
+            return s1.moves + s1.cost - s2.moves - s2.cost;
+        }
+    }
+
     private class State {
         // Each state needs to keep track of its cost and the previous state
         private Board board;
@@ -43,8 +52,10 @@ public class Solver {
      * Return the root state of a given state
      */
     private State root(State state) {
-        // TODO: Your code here
-        return null;
+        if (state.prev == null) {
+            return state;
+        }
+        return root(state.prev);
     }
 
     /*
@@ -53,7 +64,37 @@ public class Solver {
      * and a identify the shortest path to the the goal state
      */
     public Solver(Board initial) {
-        // TODO: Your code here
+        solutionState = new State(initial, 0, null);
+
+        if (isSolvable()) {
+            PriorityQueue<State> solQ = new PriorityQueue<State>(new StateComparer());
+            solutionState.cost = solutionState.board.manhattan();
+            solQ.add(solutionState);
+            HashMap<Board, Integer> visited = new HashMap<Board, Integer>();
+            visited.put(solutionState.board, 1);
+
+            boolean solved = false;
+
+            while (!solQ.isEmpty() && !solved) {
+                State temp = solQ.poll();
+                if (temp.cost == 0) {
+                    solved = true;
+                    fin = temp;
+                }
+                else {
+                    Iterable<Board> it = temp.board.neighbors(false);
+                    for (Board i : it) {
+                        if (!visited.containsKey(i)) {
+                            visited.put(i, 1);
+                            State temp2 = new State(i, temp.moves+1, temp);
+                            temp2.cost = temp2.board.manhattan();
+                            solQ.add(temp2);
+                        }
+                    }
+                }
+            }
+            minMoves = fin.moves;
+        }
     }
 
     /*
@@ -61,16 +102,21 @@ public class Solver {
      * Research how to check this without exploring all states
      */
     public boolean isSolvable() {
-        // TODO: Your code here
-        return false;
+        return solutionState.board.solvable();
     }
 
     /*
      * Return the sequence of boards in a shortest solution, null if unsolvable
      */
     public Iterable<Board> solution() {
-        // TODO: Your code here
-        return null;
+        LinkedList<Board> seq = new LinkedList<Board>();
+        State temp = fin;
+        while (temp != null) {
+            seq.addFirst(temp.board);
+            temp = temp.prev;
+        }
+        Iterable<Board> finseq = seq;
+        return finseq;
     }
 
     public State find(Iterable<State> iter, Board b) {
@@ -86,10 +132,27 @@ public class Solver {
      * Debugging space
      */
     public static void main(String[] args) {
-        int[][] initState = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+        // int[][] initState = {{1, 2, 3}, {4, 5, 6}, {7, 0, 8}}; // Easy
+        // int[][] initState = {{0, 1, 3}, {4, 2, 5}, {7, 8, 6}}; // Average
+        // int[][] initState = {{2, 3, 6}, {1, 5, 0}, {4, 7, 8}}; // Medium
+        // int[][] initState = {{0, 3, 5}, {2, 1, 8}, {4, 7, 6}}; // Hard
+        // int[][] initState = {{3, 5, 6}, {1, 2, 8}, {0, 4, 7}}; // Really hard
+        // int[][] initState = {{3, 5, 2}, {6, 0, 1}, {7, 8, 4}}; // Ridiculously hard
+        int[][] initState = {{8, 6, 7}, {2, 5, 4}, {3, 0, 1}}; // Insanely hard
         Board initial = new Board(initState);
+        long startTime = System.nanoTime();
 
         Solver solver = new Solver(initial);
+        long endTime = System.nanoTime();
+        long timeTaken = (endTime - startTime) / 1000000;
+        System.out.println("Time taken: " + timeTaken);
+        System.out.println("Steps: " + solver.minMoves);
+
+        Iterable<Board> it = solver.solution();
+
+        for (Board i : it) {
+            System.out.println(Arrays.deepToString(i.tiles));
+        }
     }
 
 
