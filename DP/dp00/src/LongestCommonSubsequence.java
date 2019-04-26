389332132a00a5fe8830459750944dee8880cd23
shashank <sh.swami235@gmail.com>
2019-04-25 21:55:51 -0400

    Adding completed DP Day 0 HW

--- DP/dp00/src/LongestCommonSubsequence.java
+++ DP/dp00/src/LongestCommonSubsequence.java
@@ -1,9 +1,32 @@
 public class LongestCommonSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: n^2
+    // Space: n^2
+    
     public static int LCS(String S1, String S2) {
-        // TODO
-        return 0;
+        int[][] lcsTab = new int[S1.length() + 1][S2.length() + 1];
+        // if S1 is null then LCS of S1, S2 =0
+        for (int i = 0; i <= S2.length(); i++) {
+            lcsTab[0][i] = 0;
+        }
+
+        // if S2 is null then LCS of S1, S2 =0
+        for (int i = 0; i <= S1.length(); i++) {
+            lcsTab[i][0] = 0;
+        }
+
+        char[] A = S1.toCharArray();
+        char[] B = S2.toCharArray();
+
+        for (int i = 1; i <= S1.length(); i++) {
+            for (int j = 1; j <= S2.length(); j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    lcsTab[i][j] = lcsTab[i - 1][j - 1] + 1;
+                } else {
+                    lcsTab[i][j] = Math.max(lcsTab[i - 1][j], lcsTab[i][j - 1]);
+                }
+            }
+        }
+        return lcsTab[S1.length()][S2.length()];
     }
 }
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,36 @@
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
-    public static int LIS(int[] A) {
-        // TODO
-        return 0;
+    // Runtime: n^2
+    // Space: n
+ 
+    public static int LIS(int [] A){
+
+        if (A.length == 0) {
+            return 0;
+        }
+ 
+        int [] lis = new int[A.length];
+        lis[0] = 1;
+        int maximo = lis[0];
+ 
+        for(int i=1; i<A.length; i++){
+            lis[i] = 1 + maximumLIS(A,i,lis);
+            maximo = Math.max(maximo, lis[i]);
+        }
+ 
+        return maximo;
+    }
+
+    private static int maximumLIS(int a[], int end, int [] lis){
+        for (int i=0; i<end; i++){
+            if( a[i] < a[end] && lis[i] > lis[end] )
+                lis[end] = lis[i];
+        }
+        return lis[end];
+    }
+
+    public static void main(String[] args) {
+        int[] A = {5, 3, 1, 7, 3, 9, 2, 1, 8, 30, 2, 18, 19, 13, 20, 7, 10, 16};
+        System.out.println("Longest increasing sybsequence : " + LIS(A));
     }
 }
